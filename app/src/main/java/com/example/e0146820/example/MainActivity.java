a4e3f5a8b1b414a08861729b5221849bd04fad0f
Naval Kumar Nanjappa <e0146820@nus.edu.sg>
2017-02-13 19:43:27 +0800

    Initial Layout

--- app/src/main/java/com/example/e0146820/example/MainActivity.java
+++ app/src/main/java/com/example/e0146820/example/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.e0146820.example;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        final TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Members"));
+        tabLayout.addTab(tabLayout.newTab().setText("Facilities"));
+        tabLayout.addTab(tabLayout.newTab().setText("Bookings"));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override public void onTabSelected(TabLayout.Tab tab) {
+
+            }
+
+            @Override public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
